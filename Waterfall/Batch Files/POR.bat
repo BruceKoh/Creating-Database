ded79d9ce372881df7423a13cec38b3a963d0504
MJ <mansfred.koh@hp.com>
2020-04-24 14:58:37 +0800

    Initial Init

---
+++ Waterfall/Batch Files/POR.bat
@@ -0,0 +1,3 @@
+call C:\Users\KohMansf\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
+call conda activate MJF
+call python "C:\Users\KohMansf\Desktop\MJ Projects\STAMS\Waterfall\POR\POR.py"
