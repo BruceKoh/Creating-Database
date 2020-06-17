8d7391113c791d5022ff06d96ddc78e1aabe836e
MJ <mansfred.koh@hp.com>
2020-06-17 10:42:49 +0800

    Added pause

--- Waterfall/Batch Files/SHIPMENT.bat
+++ Waterfall/Batch Files/SHIPMENT.bat
@@ -1,3 +1,4 @@
 call C:\Users\KohMansf\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
 call conda activate MJF
 call python "C:\Users\KohMansf\Documents\STAMS\Waterfall\SHIPMENT\Phoenix_Shipment.py"
+pause
